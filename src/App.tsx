d3dfedece93be6a3e8eb823898b2275fae7912c3
HieuTT <hieuttjob@gmail.com>
2025-07-07 16:28:52 +0700

    update chart fix ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
 import './App.css'
-
 import LineChartCustom from "./LineChartCustom.tsx";
+import {useState} from "react";
 
 
 function App() {
+    const [velocity, setVelocity] = useState(0)
+    const [peakX, setPeakX] = useState(0)
+    const [downX, setDownX] = useState(0)
+    const [durationPeak, setDurationPeak] = useState(0)
 
+    return <div className={'flex justify-center items-center bg-white text-black  '}>
+        <div className={'bg-blue-50 h-screen flex flex-col gap-4 *:p-4 p-4 max-w-[1300px] w-full'}>
+            <input onChange={(e) => setVelocity(Number(e.target.value))} value={velocity} type="number"
+                   placeholder={'Velocity'}/>
+            <input onChange={(e) => setPeakX(Number(e.target.value))} value={peakX} type="number"
+                   placeholder={'Peak X'}/>
+            <input onChange={(e) => setDownX(Number(e.target.value))} value={downX} type="number" placeholder={'Time'}/>
+            <input onChange={(e) => setDurationPeak(Number(e.target.value))} value={durationPeak} type="number"
+                   placeholder={'Duration Peak'}/>
 
-    return <div className={'grid grid-cols-12 justify-center items-start bg-white text-black h-screen w-screen '}>
-        <div className={'col-span-4 w-full h-screen'}></div>
-        <div className={'col-span-4 w-full h-screen'}></div>
-        <div className={'col-span-4   items-center justify-center flex'}>
-            <LineChartCustom velocity={1031.7} peakX={0.3} time={2.7} durationPeak={2.1}/>
         </div>
 
-        {/*<div className={'col-span-4  items-center self-center '}>*/}
-        {/*    <LineChart velocity={931.7} peakX={0.3} time={2.4} durationPeak={1.8}*/}
+
+            <LineChartCustom velocity={velocity} peakX={peakX } downX={downX} durationPeak={durationPeak}/>
+
+        {/*<div className={' col-span-4 flex items-center justify-center  '}>*/}
+        {/*    <LineChartCustom velocity={300.7} peakX={0.3} time={2.4} durationPeak={1.8}*/}
 
         {/*    />*/}
         {/*</div>*/}
--- src/LineChartCustom.tsx
+++ src/LineChartCustom.tsx
@@ -1,34 +1,80 @@
 import {useEffect, useRef, useState} from "react";
 
-const LineChartCustom = ({
+//usage
+//<LineChartCustom velocity={1530.1} peakX={0.3} downX={2.4} durationPeak={1.8}/>
 
+const LineChartCustom = ({
                              velocity,
-                             time,
+                             downX,
                              peakX,
                              durationPeak,
                          }: {
 
-    velocity: number;
-    time: number;
-    peakX: number;
-    durationPeak: number;
+    velocity: number,
+    downX: number,
+    peakX: number,
+    durationPeak: number
 }) => {
 
-    const stepX = peakX;
-    const stepY = Number((Number((velocity / 6).toFixed(0)) / 50).toFixed(0)) * 50
 
-    const lengthX = time / stepX + 1;
-    const lengthY = velocity / stepY + 1;
+    const isEnoughData = velocity > 0 && downX > 0 && peakX > 0 && durationPeak > 0
+    if (!isEnoughData) {
+        velocity = 889
+        downX = 0.5
+        peakX = 0.5
+        durationPeak = 2
+    }
 
-    const containerRef = useRef(null);
-    const [containerWidth, setContainerWidth] = useState(300);
-    const [containerHeight, setContainerHeight] = useState(450);
+    let fixedNumberY = 0;
+    const fixedNumberX = peakX % 0.1 > 0 ? 2 : 1
+
+    let stepX = peakX
+    let stepY;
+
+    if (velocity <= 1) {
+        stepY = Number((velocity / 5).toFixed(2))
+        fixedNumberY = 2
+    } else if (
+        velocity <= 10 &&
+        velocity > 1
+    ) {
+        stepY = Number((velocity / 5).toFixed(1))
+        fixedNumberY = 1
+
+    } else if (
+        velocity < 300 &&
+        velocity > 10
+    ) {
+        stepY = Number((velocity / 5).toFixed(0))
+        fixedNumberY = 0
+
+    } else {
+        stepY = Number((Number((velocity / 6).toFixed(0)) / 50).toFixed(0)) * 50
+        fixedNumberY = 0
+    }
+
+
+    const totalTime = durationPeak + peakX + downX;
+
+    let lengthX = totalTime / stepX + 1
+
+    if (lengthX > 15) {
+        lengthX = 10
+        stepX = totalTime / lengthX
+    }
+    const lengthY = (velocity / stepY) + 1
+
+    const containerRef = useRef<HTMLDivElement>(null)
+    const [containerWidth, setContainerWidth] = useState(300)
+    const [containerHeight, setContainerHeight] = useState(450)
+
     const points = [
         {x: 0, y: 0},
         {x: peakX, y: velocity},
         {x: durationPeak + peakX, y: velocity},
-        {x: time, y: 0},
+        {x: durationPeak + peakX + downX, y: 0},
     ]
+
     const maxX = stepX * lengthX
     const maxY = stepY * lengthY
 
@@ -36,24 +82,23 @@ const LineChartCustom = ({
         const update = () => {
             if (containerRef.current) {
                 if (containerRef.current.offsetWidth) {
-                    setContainerWidth(containerRef.current.offsetWidth - 40);
-                    setContainerHeight((containerRef.current.offsetWidth - 40) * 3 / 2);
+                    setContainerWidth(containerRef.current.offsetWidth - 20)
+                    setContainerHeight((containerRef.current.offsetWidth - 20) * 3 / 2)
                 } else {
-                    setContainerWidth(300);
-                    setContainerHeight(450);
+                    setContainerWidth(300)
+                    setContainerHeight(450)
                 }
-
             }
-        };
+        }
 
-        update();
-        window.addEventListener("resize", update);
-        return () => window.removeEventListener("resize", update);
-    }, []);
-    console.log(containerWidth, containerHeight)
-    const dataX = () => Array.from({length: lengthX + 1}, (_, i) => i * stepX);
+        update()
+        window.addEventListener("resize", update)
+        return () => window.removeEventListener("resize", update)
+    }, [])
 
-    const dataY = () => Array.from({length: lengthY + 1}, (_, i) => i * stepY);
+    const dataX = () => Array.from({length: lengthX + 1}, (_, i) => i * stepX)
+
+    const dataY = () => Array.from({length: lengthY + 1}, (_, i) => i * stepY)
 
     const getSvgX = (x: number) => {
         return (x / maxX) * containerWidth
@@ -68,7 +113,8 @@ const LineChartCustom = ({
             const cy = getSvgY(d.y)
 
             return <g key={`key-marker-${d.x}-${d.y}`}>
-                <text x={cx + 10} y={cy - 5} textAnchor="middle" fontSize={14} fontWeight={500}
+                <text x={cx + 10} y={cy - 5} textAnchor="middle" fontWeight={500}
+                      className={'2xl:text-[14px] xl:text-[13px] lg:text-[12px] md:text-[11px]'}
                       fill="#000">
                     {d.y}
                 </text>
@@ -85,27 +131,56 @@ const LineChartCustom = ({
         } else {
             pathD += `M ${getSvgX(points[0].x)} ${getSvgY((points[0].y))} `
         }
-        for (let i = 0; i < points.length; i++) {
-            pathD += ` L ${getSvgX(points[i].x)} ${getSvgY(points[i].y)} `
+        for (let i = 1; i < points.length - 1; i++) {
+            pathD += ` L ${getSvgX(points[i].x)} ${getSvgY(points[i].y) - 2} `
         }
 
-        return pathD;
+        pathD += ` L ${getSvgX(points[points.length - 1].x)} ${getSvgY(points[points.length - 1].y)} `
+        return pathD
     }
 
+    function renderAxisHorizontal() {
+        return dataY().map((d, i) => {
+
+            return <div key={`key-horizontal-${d}`}
+                        className={`w-full h-[0.5px] bg-gray-600  absolute left-0 `} style={{
+                bottom: (i) * 0.5 + 1 / lengthY * containerHeight * (i),
+            }}>
+            </div>
+        })
+    }
+
+    function renderAxisVertical() {
+        return dataX().map((d, i) => {
+
+            return <div key={`key-horizontal-${d}`}
+                        className={`w-[0.5px]    bg-gray-600 absolute bottom-0`} style={{
+                left: (i) * 0.1 + 1 / lengthX * containerWidth * (i),
+                height: containerHeight + 4,
+            }}>
+            </div>
+        })
+    }
 
     return <div
-        className={'h-screen lg:w-full md:w-full   lg:px-20  md:pr-5 pb-20 relative flex items-center justify-center'}>
+        className={' flex-grow flex items-center justify-center  2xl:pl-20 md:pl-14   sm:pl-14 my-auto  pr-2  pb-20'}>
         <div
-            className={`  w-full      items-center self-center relative  `} style={{height: containerHeight}}
+            className={`w-full items-center self-center relative`} style={{height: containerHeight}}
             ref={containerRef}>
             <div
-                className={'absolute -left-20 -rotate-90 -translate-y-1/2  top-1/2 font-semibold'}>Velocity
+                className={'absolute -left-20 -rotate-90 -translate-y-1/2  top-1/2   xl:text-[14px] lg:text-[13px] md:text-[12px]'}>Velocity
             </div>
-            <div className={'absolute -bottom-16 -translate-x-1/2 left-1/2   font-semibold'}>Time</div>
-            <div className={'w-[0.5px] h-full bg-black absolute bottom-0 left-0'}/>
-            <div className={'w-full h-[0.5px] bg-black absolute bottom-0 left-0'}/>
+            <div
+                className={'absolute -bottom-16 -translate-x-1/2 left-1/2     xl:text-[14px] lg:text-[13px] md:text-[12px]'}>Time
+            </div>
+            <div className={'w-[0.5px] bg-black absolute bottom-0 left-0'} style={{
+                height: containerHeight + 15,
+            }}/>
+            <div className={'  h-[0.5px] bg-black absolute bottom-0 left-0'} style={{
+                width: containerWidth + 15,
+            }}/>
 
-            <svg className="absolute inset-0  z-10 w-full h-full overflow-visible ">
+            <svg className="absolute inset-0 h-full z-10 w-full overflow-visible ">
                 <defs>
                     <marker
                         id="arrow1"
@@ -118,14 +193,16 @@ const LineChartCustom = ({
                         <path d="M 0 0 L 20 10 L 0 20 z" fill="black"/>
                     </marker>
                 </defs>
-                <path d={mathPath()} className={`fill-none `} strokeWidth={4}
-                      stroke={'#3e5efa'}/>
-                {renderMarker()}
+                {
+                    isEnoughData && <path d={mathPath()} className={`fill-none `} strokeWidth={4}
+                                          stroke={'#3e5efa'}/>
+                }
+                {isEnoughData && renderMarker()}
                 <line
                     x1="0"
-                    y1="90%"
+                    y1="99%"
                     x2="0"
-                    y2="0"
+                    y2="-15"
                     stroke="#000"
                     strokeWidth="0.5"
                     markerEnd="url(#arrow1)"
@@ -144,60 +221,44 @@ const LineChartCustom = ({
             </svg>
 
             {
-                dataX().map((d, i) => {
-
-                    return <div key={`key-horizontal-${d}`}
-                                className={`w-[0.5px] h-full  bg-black absolute bottom-0`} style={{
-                        left: (i) * 0.1 + 1 / lengthX * containerWidth * (i),
-                    }}>
-
-
-                    </div>
-                })
+                renderAxisVertical()
             }
-            {
+
+            {isEnoughData &&
                 dataX().map((d, i) => {
 
                     return <div key={`key-horizontal-number-${d}`}
-                                className={`absolute -bottom-7 -translate-x-2 lg:text-[13px] md:text-[12px]`} style={{
-                        left: (i) * 0.1 + 1 / lengthX * containerWidth * (i),
-                    }}>
-                        {d.toFixed(1)}
-
+                                className={`absolute -bottom-7 -translate-x-2  2xl:text-[14px]  xl:text-[13px] lg:text-[12px] md:text-[11px]`}
+                                style={{
+                                    left: (i) * 0.1 + 1 / lengthX * containerWidth * (i),
+                                    bottom: (lengthX > 8) ? ((i % 2 == 0) ? -20 : -40) : -20
+                                }}>
+                        {d.toFixed(fixedNumberX)}
                     </div>
                 })
             }
+
             {
+                renderAxisHorizontal()
+            }
+            {
+                isEnoughData &&
                 dataY().map((d, i) => {
 
                     return <div key={`key-horizontal-${d}`}
-                                className={`w-full h-[0.5px] bg-black absolute left-0 `} style={{
-                        bottom: (i) * 0.5 + 1 / lengthY * containerHeight * (i),
-                    }}>
-
-
-                    </div>
-                })
-            }
-            {
-                dataY().map((d, i) => {
-
-                    return <div key={`key-horizontal-${d}`}
-                                className={`absolute -left-10 translate-y-2 text-[14px]`} style={{
-                        bottom: (i) * 0.1 + 1 / lengthY * containerHeight * (i),
-                    }}>
-                        {d.toFixed(0)}
+                                className={`absolute -left-10 translate-y-2  2xl:text-[14px]  xl:text-[13px] lg:text-[12px] md:text-[11px]`}
+                                style={{
+                                    bottom: (i) * 0.1 + 1 / lengthY * containerHeight * (i),
+                                }}>
+                        {d.toFixed(fixedNumberY)}
 
                     </div>
                 })
             }
-
 
         </div>
-
     </div>
-
 
 }
 
-export default LineChartCustom;
+export default LineChartCustom
