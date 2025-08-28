e2a9f0bec576dc4da1588402387447bd80727363
HieuTT <hieuttjob@gmail.com>
2025-08-28 16:40:21 +0700

    redis ui

--- src/redis/RedisCloneUi.tsx
+++ src/redis/RedisCloneUi.tsx
@@ -1,24 +1,20 @@
 import Header from "./components/Header.tsx";
 import Sidebar from "./components/Sidebar.tsx";
-import {useState} from "react";
-
 
 const RedisCloneUi = () => {
-    const [isMinSidebar, setIsMinSidebar] = useState(false)
 
     return <div className={'min-h-screen  w-full bg-white text-black '}>
         <Header/>
         <main className={'flex flex-col relative bg-white'}>
             <div className={'flex mx-4 pt-10 gap-10 '}>
                 <div className={'w-96 h-fit'}>
-                    <Sidebar isMinSidebar={isMinSidebar} setIsMinSidebar={() => setIsMinSidebar(!isMinSidebar)}/>
+                    <Sidebar />
                 </div>
                 <div className={'flex grow overflow-y-auto bg-amber-50 text-black h-[1600px] items-end   '}>
                     <div>Content</div>
-                    <div className={`${isMinSidebar ? 'h-[600px]' : 'h-0'}`}></div>
                 </div>
                 <div className={'w-80 h-fit'}>
-                    <div className={'w-80 top-[110px]   fixed flex flex-col font-mono '}>
+                    <div className={'w-full top-[110px]   fixed flex flex-col font-mono '}>
                         <div className={'text-sm text-gray-600 '}>Edit this page</div>
                         <div className={'text-sm text-gray-600 '}>Create an issue</div>
                         <div className={'text-sm text-gray-900 '}>On this page</div>
